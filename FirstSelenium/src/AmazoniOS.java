dc4dd38c52ebb073c9a1b372e9b6a810effe9605
Pratyush Sharma <Pratyush.Sharma@mindtree.com>
2016-05-05 15:56:56 +0530

    Update AmazoniOS.java

--- FirstSelenium/src/AmazoniOS.java
+++ FirstSelenium/src/AmazoniOS.java
@@ -1,14 +1,7 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 public class AmazoniOS {
 public static void main(String[] args) throws InterruptedException {
-	WebDriver driver=new FirefoxDriver();
-	driver.manage().window().maximize();
-	driver.get("http://www.amazon.in/mobiles-accessories/b/ref=nav_shopall_sa_menu_mobile_all_mobiles?ie=UTF8&node=1389401031");
-	Thread.sleep(6000);
-	driver.findElement(By.xpath("//div[@id='FGContainer_d8656ac2d99688e40de72b10b64426bf']/div/div[2]/ul/li[3]/div/ul/li[2]/a")).click();
+	System.Out.Println("Hello");
 }
 }
